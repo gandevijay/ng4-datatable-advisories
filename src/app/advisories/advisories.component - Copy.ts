64355348301bb74249dcc7149a1262a2591d5395
Vijay Gande <Vijay9896@outlook.com>
2018-02-11 14:27:20 -0500

    Date Rage Filter is implemented, Search the list is implemented,
    Search across the list and Date Rage search is working now.
    
    Reordered the funcations in the order of execution.
    
    Cleaned up the code.

---
+++ src/app/advisories/advisories.component - Copy.ts
@@ -0,0 +1,125 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Advisory } from './advisory';
+import { DataTable, DataTableTranslations, DataTableResource } from 'angular-4-data-table';
+import { AdvisoryService } from './advisory.service';
+import { Response } from '@angular/http/src/static_response';
+import { Observable } from 'rxjs/Observable';
+import { HttpModule } from '@angular/http';
+
+@Component({
+  selector: 'app-advisories',
+  providers: [AdvisoryService],
+  templateUrl: './advisories.component.html',
+  styleUrls: ['./advisories.component.css']
+})
+export class AdvisoriesComponent {
+  private advisoryResource: DataTableResource<Advisory>;
+  private searchValue = '';
+  public advisories: Array<Advisory> = [];
+  private aryAdvisory: Advisory[];
+  advisoryCount = 0;
+  private data: Array<Advisory> = [];
+  private msg;
+  public fromDate;
+  public toDate;
+
+  translations = <DataTableTranslations>{
+    indexColumn: 'Index column',
+    expandColumn: 'Expand column',
+    selectColumn: 'Select column',
+    paginationLimit: 'Max results',
+    paginationRange: 'Showing'
+  };
+
+  @ViewChild(DataTable) advisoriesTable;
+
+  constructor(private _advisoriesService: AdvisoryService) {
+   /* this.data =  [
+      { type: 'advMeasurements', date: '05/01/2017', title: 'c181_MM_201707', link: 'http://www.bing.com'},
+      { type: 'advMeasurements', date: '07/01/2017', title: 'd181_MM_201707', link: 'http://www.google.com'},
+      { type: 'advMeasurements', date: '08/01/2017', title: 'e181_MM_201707', link: 'http://www.bing.com'},
+      { type: 'advMeasurements', date: '10/01/2017', title: 'f181_MM_201707', link: 'http://www.google.com'},
+      { type: 'advMeasurements', date: '05/01/2017', title: 'c181_MM_201707', link: 'http://www.bing.com'},
+      { type: 'advMeasurements', date: '07/01/2017', title: 'd181_MM_201707', link: 'http://www.google.com'},
+      { type: 'advMeasurements', date: '08/01/2017', title: 'e181_MM_201707', link: 'http://www.bing.com'},
+      { type: 'advMeasurements', date: '05/01/2017', title: 'c181_MM_201707', link: 'http://www.bing.com'},
+      { type: 'advMeasurements', date: '07/01/2017', title: 'd181_MM_201707', link: 'http://www.google.com'},
+      { type: 'advMeasurements', date: '08/01/2017', title: 'e181_MM_201707', link: 'http://www.bing.com'},
+      { type: 'advMeasurements', date: '05/01/2017', title: 'c181_MM_201707', link: 'http://www.bing.com'},
+      { type: 'advMeasurements', date: '07/01/2017', title: 'd181_MM_201707', link: 'http://www.google.com'},
+      { type: 'advMeasurements', date: '08/01/2017', title: 'e181_MM_201707', link: 'http://www.bing.com'},
+      { type: 'advMeasurements', date: '05/01/2017', title: 'c181_MM_201707', link: 'http://www.bing.com'},
+      { type: 'advMeasurements', date: '07/01/2017', title: 'd181_MM_201707', link: 'http://www.google.com'},
+      { type: 'advMeasurements', date: '08/01/2017', title: 'e181_MM_201707', link: 'http://www.bing.com'},
+      { type: 'advMeasurements', date: '05/01/2017', title: 'c181_MM_201707', link: 'http://www.bing.com'},
+      { type: 'advMeasurements', date: '07/01/2017', title: 'd181_MM_201707', link: 'http://www.google.com'},
+      { type: 'advMeasurements', date: '08/01/2017', title: 'e181_MM_201707', link: 'http://www.bing.com'}
+    ] as Array<Advisory>;
+    */
+        this.advisories = this.data;
+    this.advisoryResource = new DataTableResource(this.advisories);
+    this.advisoryResource.count().then(count => (this.advisoryCount = count));
+  }
+  // tslint:disable-next-line:use-life-cycle-interface
+  processQuery(vm: Advisory[]) {
+    this.advisories = vm;
+    this.advisoryResource = new DataTableResource(this.advisories);
+    this.advisoryResource.count().then(count => (this.advisoryCount = count));
+    }
+    processErrorMessage(error: any) {
+      debugger;
+      alert('Unable to process the request');
+    }
+
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngOnInit() {
+          this._advisoriesService.query()
+     .subscribe(vm => this.processQuery(vm), error => this.processErrorMessage(error)
+     );
+  }
+
+  search(params, searchValue: string): void {
+    //this.reloadAdvisories(params);
+    params.limit = 14;
+    params.offset = 0;
+    this.msg = params.offset;
+    this.searchValue = searchValue.toLowerCase();
+    if (searchValue.length >= 1) {
+      this.reloadAdvisories(params);
+    }
+    this.advisoryResource
+      .count(
+        advisory =>
+          advisory.type.toLowerCase().includes(this.searchValue) ||
+          advisory.date.toLowerCase()>this.fromDate &&
+          advisory.title.toLowerCase()<this.toDate
+      )
+      .then(count => (this.advisoryCount = count));
+  }
+
+  reloadAdvisories(params) {
+    this.msg = params.limit;
+    this.advisoryResource
+      .query(params,
+        advisory =>
+          advisory.type.toLowerCase().includes(this.searchValue) ||
+          advisory.date.toLowerCase()>this.fromDate &&
+          advisory.title.toLowerCase()<this.toDate
+      )
+      .then(advisory => (this.advisories = advisory));
+    // debugger;
+    //   let finalResult = this.advisories.filter(r=>r.type.includes(this.searchValue));
+    //   if(this.fromDate !== '')
+    //   {
+    //     finalResult = finalResult.filter(r=> r.date > this.fromDate);
+    //   }
+    //   if(this.toDate !== ''){
+    //   finalResult=finalResult.filter(r=>r.date<this.toDate);
+    // }
+
+
+    //   this.advisoryResource = new DataTableResource(this.advisories);
+    // this.advisoryResource.count().then(count => (this.advisoryCount = count));
+    // this.getDatabyHttp();
+  }
+}
--- src/app/advisories/advisories.component.ts
+++ src/app/advisories/advisories.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Advisory } from './advisory';
-import { DataTable, DataTableTranslations, DataTableResource } from 'angular-4-data-table';
+import { DataTable, DataTableTranslations, DataTableResource, DataTableParams } from 'angular-4-data-table';
 import { AdvisoryService } from './advisory.service';
 import { Response } from '@angular/http/src/static_response';
 import { Observable } from 'rxjs/Observable';
 import { HttpModule } from '@angular/http';
+import { take } from 'rxjs/operators/take';
 
 @Component({
   selector: 'app-advisories',
@@ -16,13 +17,13 @@ export class AdvisoriesComponent {
   private advisoryResource: DataTableResource<Advisory>;
   private searchValue = '';
   public advisories: Array<Advisory> = [];
-  private aryAdvisory: Advisory[];
+  //private aryAdvisory: Advisory[];
   advisoryCount = 0;
   private data: Array<Advisory> = [];
   private msg;
-  public fromDate;
-  public toDate;
-
+  public fromDate = "";
+  public toDate = "";
+  public params: DataTableParams;
   translations = <DataTableTranslations>{
     indexColumn: 'Index column',
     expandColumn: 'Expand column',
@@ -34,92 +35,99 @@ export class AdvisoriesComponent {
   @ViewChild(DataTable) advisoriesTable;
 
   constructor(private _advisoriesService: AdvisoryService) {
-   /* this.data =  [
-      { type: 'advMeasurements', date: '05/01/2017', title: 'c181_MM_201707', link: 'http://www.bing.com'},
-      { type: 'advMeasurements', date: '07/01/2017', title: 'd181_MM_201707', link: 'http://www.google.com'},
-      { type: 'advMeasurements', date: '08/01/2017', title: 'e181_MM_201707', link: 'http://www.bing.com'},
-      { type: 'advMeasurements', date: '10/01/2017', title: 'f181_MM_201707', link: 'http://www.google.com'},
-      { type: 'advMeasurements', date: '05/01/2017', title: 'c181_MM_201707', link: 'http://www.bing.com'},
-      { type: 'advMeasurements', date: '07/01/2017', title: 'd181_MM_201707', link: 'http://www.google.com'},
-      { type: 'advMeasurements', date: '08/01/2017', title: 'e181_MM_201707', link: 'http://www.bing.com'},
-      { type: 'advMeasurements', date: '05/01/2017', title: 'c181_MM_201707', link: 'http://www.bing.com'},
-      { type: 'advMeasurements', date: '07/01/2017', title: 'd181_MM_201707', link: 'http://www.google.com'},
-      { type: 'advMeasurements', date: '08/01/2017', title: 'e181_MM_201707', link: 'http://www.bing.com'},
-      { type: 'advMeasurements', date: '05/01/2017', title: 'c181_MM_201707', link: 'http://www.bing.com'},
-      { type: 'advMeasurements', date: '07/01/2017', title: 'd181_MM_201707', link: 'http://www.google.com'},
-      { type: 'advMeasurements', date: '08/01/2017', title: 'e181_MM_201707', link: 'http://www.bing.com'},
-      { type: 'advMeasurements', date: '05/01/2017', title: 'c181_MM_201707', link: 'http://www.bing.com'},
-      { type: 'advMeasurements', date: '07/01/2017', title: 'd181_MM_201707', link: 'http://www.google.com'},
-      { type: 'advMeasurements', date: '08/01/2017', title: 'e181_MM_201707', link: 'http://www.bing.com'},
-      { type: 'advMeasurements', date: '05/01/2017', title: 'c181_MM_201707', link: 'http://www.bing.com'},
-      { type: 'advMeasurements', date: '07/01/2017', title: 'd181_MM_201707', link: 'http://www.google.com'},
-      { type: 'advMeasurements', date: '08/01/2017', title: 'e181_MM_201707', link: 'http://www.bing.com'}
-    ] as Array<Advisory>;
-    */
-        this.advisories = this.data;
+    // this.getStaticData(); // Uncomment the follow statements for loading the data from memory.
+    this.advisories = this.data;
     this.advisoryResource = new DataTableResource(this.advisories);
     this.advisoryResource.count().then(count => (this.advisoryCount = count));
   }
-  // tslint:disable-next-line:use-life-cycle-interface
-  processQuery(vm: Advisory[]) {
-    this.advisories = vm;
-    this.advisoryResource = new DataTableResource(this.advisories);
-    this.advisoryResource.count().then(count => (this.advisoryCount = count));
-    }
-    processErrorMessage(error: any) {
-      debugger;
-      alert('Unable to process the request');
-    }
+
+  getStaticData() {
+    this.data = [
+      { type: 'advMeasurements', date: '05/01/2017', title: 'c181_MM_201707', link: 'http://www.bing.com' },
+      { type: 'advMeasurements', date: '07/01/2017', title: 'd181_MM_201707', link: 'http://www.google.com' },
+      { type: 'advMeasurements', date: '08/01/2017', title: 'e181_MM_201707', link: 'http://www.bing.com' },
+      { type: 'advMeasurements', date: '10/01/2017', title: 'f181_MM_201707', link: 'http://www.google.com' },
+      { type: 'advMeasurements', date: '05/01/2017', title: 'c181_MM_201707', link: 'http://www.bing.com' },
+      { type: 'advMeasurements', date: '07/01/2017', title: 'd181_MM_201707', link: 'http://www.google.com' },
+      { type: 'advMeasurements', date: '08/01/2017', title: 'e181_MM_201707', link: 'http://www.bing.com' },
+      { type: 'advMeasurements', date: '05/01/2017', title: 'c181_MM_201707', link: 'http://www.bing.com' },
+      { type: 'advMeasurements', date: '07/01/2017', title: 'd181_MM_201707', link: 'http://www.google.com' },
+      { type: 'advMeasurements', date: '08/01/2017', title: 'e181_MM_201707', link: 'http://www.bing.com' },
+      { type: 'advMeasurements', date: '05/01/2017', title: 'c181_MM_201707', link: 'http://www.bing.com' },
+      { type: 'advMeasurements', date: '07/01/2017', title: 'd181_MM_201707', link: 'http://www.google.com' },
+      { type: 'advMeasurements', date: '08/01/2017', title: 'e181_MM_201707', link: 'http://www.bing.com' },
+      { type: 'advMeasurements', date: '05/01/2017', title: 'c181_MM_201707', link: 'http://www.bing.com' },
+      { type: 'advMeasurements', date: '07/01/2017', title: 'd181_MM_201707', link: 'http://www.google.com' },
+      { type: 'advMeasurements', date: '08/01/2017', title: 'e181_MM_201707', link: 'http://www.bing.com' },
+      { type: 'advMeasurements', date: '05/01/2017', title: 'c181_MM_201707', link: 'http://www.bing.com' },
+      { type: 'advMeasurements', date: '07/01/2017', title: 'd181_MM_201707', link: 'http://www.google.com' },
+      { type: 'advMeasurements', date: '08/01/2017', title: 'e181_MM_201707', link: 'http://www.bing.com' }
+    ] as Array<Advisory>;
+  }
 
   // tslint:disable-next-line:use-life-cycle-interface
   ngOnInit() {
-          this._advisoriesService.query()
-     .subscribe(vm => this.processQuery(vm), error => this.processErrorMessage(error)
-     );
+    this._advisoriesService.query()
+      .subscribe(data => this.processQuery(data), error => this.processErrorMessage(error)
+      );
+    this.params = { sortBy: 'date' };
   }
 
-  search(params, searchValue: string): void {
-    //this.reloadAdvisories(params);
+  // This will execute when http.get returns the data.
+  processQuery(data: Advisory[]) {
+    this.advisories = data;
+    this.advisoryResource = new DataTableResource(this.advisories);
+    this.advisoryResource.count().then(count => (this.advisoryCount = count));
+  }
+
+  // This will run when http returns error.
+  processErrorMessage(error: any) {
+    debugger;
+    alert('Unable to process the request');
+  }
+
+  //Summary: Searches the rows for matching conditions / filter conditions, and binds the filtered result set to data table.
+  search(params): void {
+    debugger;
     params.limit = 14;
     params.offset = 0;
     this.msg = params.offset;
-    this.searchValue = searchValue.toLowerCase();
-    if (searchValue.length >= 1) {
-      this.reloadAdvisories(params);
-    }
+    this.reloadAdvisories(params);
     this.advisoryResource
       .count(
-        advisory =>
-          advisory.type.toLowerCase().includes(this.searchValue) ||
-          advisory.date.toLowerCase()>this.fromDate &&
-          advisory.title.toLowerCase()<this.toDate
+      advisory =>
+        this.filterRow(advisory)
       )
       .then(count => (this.advisoryCount = count));
   }
 
+  //loads the filtered advisories into the array.
   reloadAdvisories(params) {
-    this.msg = params.limit;
     this.advisoryResource
       .query(params,
-        advisory =>
-          advisory.type.toLowerCase().includes(this.searchValue) ||
-          advisory.date.toLowerCase()>this.fromDate &&
-          advisory.title.toLowerCase()<this.toDate
+      advisory =>
+        this.filterRow(advisory)
       )
       .then(advisory => (this.advisories = advisory));
-    // debugger;
-    //   let finalResult = this.advisories.filter(r=>r.type.includes(this.searchValue));
-    //   if(this.fromDate !== '')
-    //   {
-    //     finalResult = finalResult.filter(r=> r.date > this.fromDate);
-    //   }
-    //   if(this.toDate !== ''){
-    //   finalResult=finalResult.filter(r=>r.date<this.toDate);
-    // }
-
-
-    //   this.advisoryResource = new DataTableResource(this.advisories);
-    // this.advisoryResource.count().then(count => (this.advisoryCount = count));
-    // this.getDatabyHttp();
+    // debugger; ToDo: Will execute till this point and breaks.
   }
+
+  // Summary: This funcation validates the input search text as well as entered data rage. to make sure they are part of the Adviroy results
+  // Returns: True if criteria matches else False.
+  filterRow(advisory: Advisory) {
+    var typeVal = true;
+    var dateVal = true;
+
+    if (this.searchValue !== '')
+      typeVal = advisory.type.toLowerCase().includes(this.searchValue.toLowerCase()) || advisory.title.toLowerCase().includes(this.searchValue.toLowerCase());
+
+    if (this.fromDate !== "" && this.toDate !== "") {
+      var fromD = new Date(this.fromDate);
+      var toD = new Date(this.toDate);
+      var D = new Date(advisory.date);
+      dateVal = D >= fromD && D <= toD;
+    }
+    return typeVal && dateVal;
+  }
+
 }
